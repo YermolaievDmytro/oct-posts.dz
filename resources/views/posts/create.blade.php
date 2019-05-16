7668ed5b3dd14d368fb2bffb91feee6cd0070ad9
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2019-05-16 14:26:56 +0300

    min-func-ok

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,36 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-  <!-- Bootstrap шаблон... -->
-  
-  <div class="panel-body">
+
+<div class="panel-body">
     <!-- Отображение ошибок проверки ввода -->
     @include('common.errors')
     <!-- Форма новой задачи -->
     <form action="{{ route('posts.store') }}" method="POST" class="form-horizontal">
-      {{ csrf_field() }}
-      <!-- Имя задачи -->
-      <div class="form-group">
-        <label  class="col-sm-3 control-label">Название</label>
-        <div class="col-sm-6">
-          <input type="text" name="name" id="post-name" class="form-control">
+        {{ csrf_field() }}
+        <!-- Имя задачи -->
+        <div >        
+            <div style="width: 50%;" class="sm-6">
+                <label  class="col-sm-3 control-label">Название</label>
+                <input type="text" name="name" id="post-name" class="form-control">
+            </div>
+            <div class="sm-6">
+                <label  class="col-sm-3 control-label">Текст</label>
+                <textarea name="text" id="post-text" class="form-control"></textarea>
+            </div>
         </div>
-        <div class="col-sm-6">
-            <label  class="col-sm-3 control-label">Текст</label>
-            <textarea name="text" id="post-text" class="form-control">
-                
-            </textarea>
+        <!-- Кнопка добавления задачи -->
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-default">
+                    <i class="fa fa-plus"></i> Добавить задачу
+                </button>
+            </div>
         </div>
-      </div>
-      <!-- Кнопка добавления задачи -->
-      <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-6">
-          <button type="submit" class="btn btn-default">
-            <i class="fa fa-plus"></i> Добавить задачу
-          </button>
-        </div>
-      </div>
     </form>
-  </div>
-  <!-- TODO: Текущие задачи -->
+</div>
+<!-- TODO: Текущие задачи -->
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,14 @@
 @section('content')
 <div class="form-group">
     <label  class="col-sm-3 control-label">Название</label>
-    <div class="col-sm-6">
+    <div class="sm-6">
         <input type="text" readonly class="form-control" value="{{$post->name}}">
     </div>
-    <div class="col-sm-6">
+    <div class="sm-6">
         <label  class="col-sm-3 control-label">Текст</label>
-        <textarea name="text" id="post-text" class="form-control">
-                {{$post->text}}
-        </textarea>
+        <textarea name="text" id="post-text" class="form-control" readonly>{{$post->text}}</textarea>
     </div>
-    <form action="{{ route('posts.index') }}" method="POST">
+    <form action="{{ route('posts.index') }}" method="POST" style="margin-top: 5px;">
         {{ csrf_field() }}
         {{ method_field('GET') }}
 
